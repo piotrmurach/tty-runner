5d55c52670b3584129a7b465e70a514107002f4e
Piotr Murach <pmurach@gmail.com>
2020-10-31 17:20:31 +0100

    Add example to show different ways to run commands

---
+++ examples/commands.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-runner"
+
+class AddCommand
+  def call
+    puts "config adding..."
+  end
+end
+
+class GetCommand
+  def call
+    puts "config getting..."
+  end
+end
+
+class RemoveCommand
+  def execute
+    puts "config removing..."
+  end
+end
+
+class App < TTY::Runner
+  commands do
+    on "config" do
+      on "add", run: AddCommand
+
+      on :get, run: "get_command"
+
+      on "remove", aliases: %w[rm] do
+        run "remove_command#execute"
+      end
+
+      on "edit" do
+        run { |out| out.puts "config editing..." }
+      end
+    end
+
+    on "tag" do
+      on "create", run: -> { puts "tag creating..." }
+
+      on "delete", run: -> { puts "tag deleting..." }
+    end
+  end
+end
+
+App.run
