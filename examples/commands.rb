d500cb03446c1015a8cfda89032525e8e93cccf7
Piotr Murach <pmurach@gmail.com>
2020-11-12 22:01:24 +0100

    Add descriptions for all the commands in the commands example

--- examples/commands.rb
+++ examples/commands.rb
@@ -24,24 +24,26 @@ end
 
 class App < TTY::Runner
   commands do
-    on "config" do
-      on "add", run: Config::AddCommand
+    on "config", "Manage config file" do
+      on "add", "Add a new entry", run: Config::AddCommand
 
-      on :get, run: "get_command"
+      on :get, "Get value for a key", run: "get_command"
 
       on "remove", aliases: %w[rm] do
+        desc "Remove an entry"
+
         run "remove_command#execute"
       end
 
-      on "edit" do
+      on "edit", "Open an editor" do
         run { puts "config editing..." }
       end
     end
 
-    on "tag" do
-      on "create", run: -> { puts "tag creating..." }
+    on "tag", "Manage tags" do
+      on "create", "Add a new tag object", run: -> { puts "tag creating..." }
 
-      on "delete", run: -> { puts "tag deleting..." }
+      on "delete", "Delete a tag object", run: -> { puts "tag deleting..." }
     end
   end
 end
